a40366bd8f5234e6bd51c9252e66995583c5f481
Tom Lord <tom@showmyhomework.co.uk>
2018-01-23 22:45:14 +0000

    Implement error checking for 'I' command

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,25 +1,48 @@
 class BitmapEditor
+  InvalidInputError = Class.new(StandardError)
   def run(file)
     return puts 'please provide correct file' if file.nil? || !File.exist?(file)
 
     File.open(file).each do |line|
       line = line.chomp
-      case line
+      case line[0]
       when 'I'
-        puts 'I N M - Create a new M x N image with all pixels coloured white (O)'
+        validate_line_i(line)
+        # @image = BitmapImage.new(line) # TODO
+        # 'I N M - Create a new M x N image with all pixels coloured white (O)'
       when 'C'
-        puts 'C - Clears the table, setting all pixels to white (O)'
+        # 'C - Clears the table, setting all pixels to white (O)'
       when 'L'
-        puts 'L X Y C - Colours the pixel (X,Y) with colour C'
+        # 'L X Y C - Colours the pixel (X,Y) with colour C'
       when 'V'
-        puts 'V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).'
+        # 'V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).'
       when 'H'
-        puts 'H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).'
+        # 'H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).'
       when 'S'
-        puts 'Show the contents of the current image'
+        # 'Show the contents of the current image'
       else
-        puts 'unrecognised command :('
+        fail_with_error('Unrecognised command', line)
       end
     end
+
+  rescue InvalidInputError => e
+    warn(e)
+  end
+
+  private
+
+  def validate_line_i(line)
+    arguments = line.split
+    fail_with_error('Wrong number of arguments', line) unless arguments.count == 3
+    fail_with_error('Unrecognised command', line) unless arguments[0] == 'I'
+    arguments[1..2].each do |coordinate|
+      unless coordinate =~ /\A\d{1,3}\z/ && coordinate.to_i.between?(1, 250)
+        fail_with_error('Input invalid or out of range (1-250)', line)
+      end
+    end
+  end
+
+  def fail_with_error(message, line)
+    raise(InvalidInputError, "ERROR - #{message}: '#{line}'")
   end
 end
