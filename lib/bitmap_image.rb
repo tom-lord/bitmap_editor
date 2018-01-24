b05ab4fa3942b58314ed17ec2ee1c4b467940031
Tom Lord <tom@showmyhomework.co.uk>
2018-01-24 23:45:34 +0000

    Refactor to store explicit height and width

--- lib/bitmap_image.rb
+++ lib/bitmap_image.rb
@@ -1,6 +1,12 @@
 class BitmapImage
-  attr_reader :data
+  attr_reader :height, :width, :data
   def initialize(height, width)
+    @height = height
+    @width = width
+    clear
+  end
+
+  def clear
     @data = Array.new(height.to_i) { Array.new(width.to_i) { 'O' } }
   end
 
