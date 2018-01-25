b8e8eec2a58a0a78cc9cff5500292c2dd5bd893b
Tom Lord <tom@showmyhomework.co.uk>
2018-01-25 20:29:48 +0000

    Implement change_horizontal and change_vertical

--- lib/bitmap_image.rb
+++ lib/bitmap_image.rb
@@ -17,4 +17,16 @@ class BitmapImage
   def change_pixel(x, y, colour)
     @data[y.to_i - 1][x.to_i - 1] = colour
   end
+
+  def change_vertical(column, row_start, row_end, colour)
+    (row_start.to_i..row_end.to_i).each do |row|
+      change_pixel(column.to_i, row, colour)
+    end
+  end
+
+  def change_horizontal(row, column_start, column_end, colour)
+    (column_start.to_i..column_end.to_i).each do |column|
+      change_pixel(column, row.to_i, colour)
+    end
+  end
 end
