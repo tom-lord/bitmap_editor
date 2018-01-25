39348fc67357b03e22d2c723e857efb7f07e897c
Tom Lord <tom@showmyhomework.co.uk>
2018-01-25 00:17:31 +0000

    Valid parser spec including C command

--- spec/valid_input_spec.rb
+++ spec/valid_input_spec.rb
@@ -47,4 +47,22 @@ describe 'BitmapEditor#run with invalid input' do
     end
     run_valid_output_spec
   end
+
+  context 'initialize, change pixel, clear and show' do
+    let(:input_string) do
+      <<~INPUT
+        I 2 3
+        L 2 1 X
+        C
+        S
+      INPUT
+    end
+    let(:expected_output) do
+      <<~OUTPUT
+        O O O
+        O O O
+      OUTPUT
+    end
+    run_valid_output_spec
+  end
 end
